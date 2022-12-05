551806d5208d7664cfbd25cbf68c2314f7985e1c
Marian <rozumnyi.marian@gmail.com>
2022-12-05 22:08:12 +0200

    Add new task "Book"

--- Essential/Class/Book.cs
+++ Essential/Class/Book.cs
@@ -1,14 +1,55 @@
 ﻿using System;
-namespace Essential.Class
+namespace Essential.Tasks
 {
-    public class Book
+    //Используя Visual Studio, создайте проект по шаблону Console Application.
+    //Требуется:
+    //Создать класс Book.Создать классы Title, Author и Content, каждый из которых должен содержать одно строковое поле и метод void Show().
+    //Реализуйте возможность добавления в книгу названия книги, имени автора и содержания.
+    //Выведите на экран разными цветами при помощи метода Show() название книги, имя автора и содержание.
+    class Program
     {
-        public Book()
+        public partial class Book
         {
-            Console.WriteLine("Hello Marian");
-            //add
-            //adddddd
-            //add  333
+            public Title Title { get; set; }
+            public Author Author { get; set; }
+            public Content Content { get; set; }
+
+            public void Show()
+            {
+                Title.Show();
+                Author.Show();
+                Content.Show();
+            }
+        }
+        public partial class Title
+        {
+            public string title = "My Life";
+            public void Show()
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(title);
+                Console.ResetColor();
+            }
+        }
+        public partial class Author
+        {
+            public string author = "Marian Rozumnyi";
+            public void Show()
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine(author);
+                Console.ResetColor();
+            }
+        }
+        public partial class Content
+        {
+            public string content = "Hobby";
+            public void Show()
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine(content);
+                Console.ResetColor();
+            }
         }
     }
 }
