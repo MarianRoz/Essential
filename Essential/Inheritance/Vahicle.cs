eae289d1e3c73163041351cde1bc6a341e705167
Marian <rozumnyi.marian@gmail.com>
2022-12-11 15:13:58 +0200

    Add new task

---
+++ Essential/Inheritance/Vahicle.cs
@@ -0,0 +1,59 @@
+﻿using System;
+namespace Essential.Inheritance
+{
+    //    Задание 3
+    //Используя Visual Studio, создайте проект по шаблону Console Application.
+    //Требуется:
+    //Создать класс Vehicle.
+    //В теле класса создайте поля: координаты и параметры средств передвижения (цена, скорость, год выпуска).
+    //Создайте 3 производных класса Plane, Саг и Ship.
+    //Для класса Plane должна быть определена высота и количество пассажиров.
+    //Для класса Ship — количество пассажиров и порт приписки.
+    //Написать программу, которая выводит на экран информацию о каждом средстве передвижения.
+
+    public class Vehicle
+    {
+        public int prise;
+        public int speed;
+        public int year;
+    }
+    public class Plane : Vehicle
+    {
+        public Plane(int prise, int speed, int year)
+        {
+            this.prise = prise;
+            this.speed = speed;
+            this.year = year;
+        }
+        public void Method(int height, int passengersPlane)
+        {
+            Console.WriteLine($"Plane: prise-{prise} $, speed-{speed} km/h, year-{year}, height-{height} meters, passengers plane-{passengersPlane} people");
+        }
+    }
+    class Car : Vehicle
+    {
+        public Car(int prise, int speed, int year)
+        {
+            this.prise = prise;
+            this.speed = speed;
+            this.year = year;
+        }
+        public void Method()
+        {
+            Console.WriteLine($"Car: prise-{prise} $, speed-{speed} km/h, year-{year}");
+        }
+    }
+    class Ship : Vehicle
+    {
+        public Ship(int prise, int speed, int year)
+        {
+            this.prise = prise;
+            this.speed = speed;
+            this.year = year;
+        }
+        public void Method(int homePort, int passengersPlane)
+        {
+            Console.WriteLine($"Ship: prise-{prise} $, speed-{speed} km/h, year-{year}, home port-{homePort}, passengers plane-{passengersPlane} people");
+        }
+    }
+}
