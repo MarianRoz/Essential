9602ab0363eb7656a1906f8a7f12176f24673a64
Marian <rozumnyi.marian@gmail.com>
2022-12-26 11:15:19 +0200

    Add new task

---
+++ Essential/StaticClasses/Sort.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Essential.StaticClasses
+{
+//    Задание 4
+//Используя Visual Studio, создайте проект по шаблону Console Application.
+//Требуется: Создать расширяющий метод для целочисленного массива, который сортирует элементы массива по возрастанию.
+
+    public static class Sort
+    {
+        public static void Sorted(int[] ar)
+        {
+            for (int i = 0; i < ar.Length; i++)
+            {
+                for (int j = 0; j < ar.Length-1; j++)
+                {
+                    if (ar[j] > ar[j + 1])
+                    {
+                        int z = ar[j];
+                        ar[j] = ar[j + 1];
+                        ar[j + 1] = z;
+                    }
+                }
+            }
+        }
+    }
+}
