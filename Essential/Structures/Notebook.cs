599bc826740130a7e71cd316014c5f3e6e18386b
Marian <rozumnyi.marian@gmail.com>
2022-12-30 17:17:35 +0200

    Add new task

---
+++ Essential/Structures/Notebook.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Essential.Structures
+{
+//    Задание
+//Используя Visual Studio, создайте проект по шаблону Console Application.
+//Создайте структуру с именем - Notebook.
+//Поля структуры: модель, производитель, цена.
+//В структуре должен быть реализован конструктор для инициализации полей и метод для вывода содержимого полей на экран.
+
+    struct Notebook
+    {
+        int model;
+        int manufacturer;
+        int price;
+
+        public Notebook(int model, int manufacturer, int price)
+        {
+            this.model = model;
+            this.manufacturer = manufacturer;
+            this.price = price;
+        }
+        public void Show()
+        {
+            Console.WriteLine($"{model}\n{manufacturer}\n{price}");
+        }
+    }
+}
