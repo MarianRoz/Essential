01c89b1a3fdeb602f24f138a34f14b04d8bd295d
Marian <rozumnyi.marian@gmail.com>
2023-01-03 20:50:28 +0200

    Add new task

---
+++ Essential/Structures/DataTime.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Essential.Structures
+{
+//    Задание
+//Используя Visual Studio, создайте проект по шаблону Console Application.
+//Реализуйте программу, которая будет принимать от пользователя дату его рождения и выводить количество дней до его следующего дня рождения.
+    struct DataTime
+    {
+        public DataTime(DateTime hB)
+        {
+            DateTime today = DateTime.Now;
+            TimeSpan left = hB - today;
+            Console.WriteLine("My birthday will be in " +left.Days+ " days and " +left.Hours+ " hours");
+        }
+    }
+}
