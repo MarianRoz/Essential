707f9abc0ac9973852fe8b3472c0c052c8b44561
Marian <rozumnyi.marian@gmail.com>
2023-01-11 23:02:56 +0200

    Add new task

---
+++ Essential/010_Generics/MyClass<T>.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Essential._Generics
+{
+    //    Задание
+    //Используя Visual Studio, создайте проект по шаблону Console Application.
+    //Создайте класс MyClass<T>, содержащий статический фабричный метод - T FacrotyMethod(),
+    //который будет порождать экземпляры типа, указанного в качестве параметра типа(указателя места заполнения типом – Т).
+
+    public class MyClass<T> where T : new()
+    {
+        public static T FacrotyMethod()
+        { 
+            return new T();
+        }
+    }
+}
+//namespace Essential
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            MyClass<int> a = new MyClass<int>();
+//            Console.WriteLine(a.GetHashCode());
+//        }
+//    }
+//}
