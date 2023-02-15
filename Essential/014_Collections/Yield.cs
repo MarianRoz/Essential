e055b6759f03a2315153e1e7825f33b18de9628b
Marian <rozumnyi.marian@gmail.com>
2023-02-15 23:21:05 +0200

    Add new task

---
+++ Essential/014_Collections/Yield.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+
+namespace Essential._Collections
+{
+//    Задание
+//Используя Visual Studio, создайте проект по шаблону Console Application.
+//Создайте метод, который в качестве аргумента принимает массив целых чисел и возвращает коллекцию всех четных чисел массива.
+//Для формирования коллекции используйте оператор yield.
+    class Yield : IEnumerable
+    {
+        int[] array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 ,10 };
+        
+        public IEnumerator GetEnumerator()
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i]%2==0)
+                {
+                    yield return array[i];
+                    Console.WriteLine(array[i]);
+                }
+            }
+        }
+    }
+}
