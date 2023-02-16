e502c4bcb85488c6fc9840bc055c7680671f8fad
Marian <rozumnyi.marian@gmail.com>
2023-02-16 20:12:40 +0200

    Add new task

---
+++ Essential/014_Collections/ExtensionMethods.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Essential._Collections
+{
+//    Задание 4
+//Используя Visual Studio, создайте проект по шаблону Console Application.
+//Создайте расширяющий метод: public static T[] GetArray<T>(this IEnumerable<T> list) { ...}
+//Примените расширяющий метод к экземпляру типа MyList<T>, разработанному в домашнем задании 2 для данного урока.
+//Выведите на экран значения элементов массива, который вернул расширяющий метод GetArray().
+
+    public static class ExtensionMethods
+    {
+        public static T[] GetArray<T>(this IEnumerable<T> list)
+        {
+            T[] array = new T[list.Count()];
+
+            int i = 0;
+
+            foreach (T item in list)
+            {
+                array[i] = item;
+                i++;
+            }
+            return array;
+        }
+    }
+}
