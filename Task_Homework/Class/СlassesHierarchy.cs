29da0acdec70b6fa9f31ae4d0daa844f3c68c544
Marian <rozumnyi.marian@gmail.com>
2023-02-25 00:11:03 +0200

    Add new homework

---
+++ Task_Homework/Class/СlassesHierarchy.cs
@@ -0,0 +1,67 @@
+﻿using System;
+namespace Task_Homework.Class
+{
+    //Треба класно створити ієрархію наступних класів
+    //Кіт пес кролик ворона і літак
+    //Кіт пес кролик ворона мають властивість ім’я і метод йти
+    //Ворона має ще метод літати
+    //А літак має властивість ім’я і метод літати
+    public abstract class СlassesHierarchy
+    {
+        string name;
+        public abstract string Name
+        { get; set; }
+        public virtual void Go()
+        { }
+        public virtual void Fly()
+        { }
+    }
+    public class Cat : СlassesHierarchy
+    {
+        string name;
+        public override string Name
+        { get { return name; } set { name = value; } }
+        public override void Go()
+        {
+            Console.WriteLine("Cat can walk");
+        }
+    }
+    public class Dog : СlassesHierarchy
+    {
+        string name;
+        public override string Name
+        { get { return name; } set { name = value; } }
+        public override void Go()
+        {
+            Console.WriteLine("Dog can walk");
+        }
+    }
+    public class Rabbit : СlassesHierarchy
+    {
+        string name;
+        public override string Name
+        { get { return name; } set { name = value; } }
+        public override void Go()
+        {
+            Console.WriteLine("Rabbit can walk");
+        }
+    }
+    public class Crow : СlassesHierarchy
+    {
+        string name;
+        public override string Name
+        { get { return name; } set { name = value; } }
+        public override void Go()
+        { Console.WriteLine("Сrow can walk"); }
+        public override void Fly()
+        { Console.WriteLine("Сrow can fly"); }
+    }
+    public class Plane : СlassesHierarchy 
+    {
+        string name;
+        public override string Name
+        { get { return name; } set { name = value; } }
+        public override void Fly()
+        { Console.WriteLine("Plane can fly"); }
+    }
+}
---
+++ Task_Homework/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Task_Homework.Class;
+
+namespace Task_Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat();
+            Console.WriteLine(cat.Name = "Name: Myrchik");
+            cat.Go();
+
+            Dog dog = new Dog();
+            Console.WriteLine(dog.Name = "Name: Myhtar");
+            dog.Go();
+
+            Rabbit rabbit = new Rabbit();
+            Console.WriteLine(rabbit.Name = "Name: Rabbitok");
+            rabbit.Go();
+
+            Crow crow = new Crow();
+            Console.WriteLine(crow.Name = "Name: Black");
+            crow.Go();
+            crow.Fly();
+
+            Plane plane = new Plane();
+            Console.WriteLine(plane.Name = "Name: AirLine");
+            plane.Fly();
+        }
+    }
+}
