be5d6cc374fa7e75253eab6ace33bd3979d3b8ab
Marian <rozumnyi.marian@gmail.com>
2023-08-02 16:22:15 +0300

    Add new task

---
+++ Essentian_2/018_Namespaces/MyNamespace.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+//Задание 1
+//Создайте собственное пространство имен MyNamespace с классом MyClass и подключите его в другом приложении.
+namespace Apartment
+{
+    using Apartment.Toilet;
+    using Apartment.Bedroom;
+
+    namespace Toilet
+    {
+        class Rosette
+        {
+            public void On() { Console.WriteLine("The light in toilet on"); }
+            public void Off() { Console.WriteLine("The light in toilet off"); }
+        }
+    }
+
+    namespace Bedroom
+    {
+        class Rosette
+        {
+            public void On() { Console.WriteLine("The light in bedroom on"); }
+            public void Off() { Console.WriteLine("The light in bedroom off"); }
+        }
+    }
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Apartment.Toilet.Rosette bulbToilet = new Apartment.Toilet.Rosette();
+    //        bulbToilet.On();
+    //        Apartment.Bedroom.Rosette bulbBedroom = new Apartment.Bedroom.Rosette();
+    //        bulbBedroom.On();
+    //    }
+    //}
+}
+
+
